447e2eb74bc7938a78822b87cd9dddbce20994ba
EnzoHideki <enzo25estrela@gmail.com>
2016-08-15 17:29:03 -0300

    Add files via upload
    Adicionando header pessoal (Enzo)

---
+++ start/enzo.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define dbg(args...) fprintf(stderr, args)
+#define dbc(x) cerr << x << "\n"
+#define dbn(x) cerr << #x << " == " << x << "\n"
+#define endl '\n'
+#define bits(x) __builtin_popcount(x)
+#define m(x) memset((x),0,sizeof(x))
+#define minf(x) memset((x),63,sizeof(x))
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef set<int> si;
+typedef set<pii> sii;
+
+const int MAXM = 1e4 + 10;
+const int MAXN = 1e3 + 10;
+const int INF = 0x3f3f3f3f;
+
+//
+
+int main(){
+
+}
